862e843677676ef969b9bab0ec944bdd2c59ff3e
HussamCheema <coms.copp@gmail.com>
2022-04-10 21:18:10 +0500

    Created Models and basic template

--- base/models.py
+++ base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
--- base/views.py
+++ base/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .models import Post
 
 def home(request):
-    
+
     context = {
 
     }
     return render(request, 'base/home.html', context)
+
+
+def about(request):
+    
+    context = {
+
+    }
+    return render(request, 'base/about.html', context)
+
+
+def contact(request):
+    
+    context = {
+
+    }
+    return render(request, 'base/contact.html', context)
